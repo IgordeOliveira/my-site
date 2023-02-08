30f71ba2581b91a50d45ec37882e284e19d97656
IgorOliveira <igordeeoliveira@gmail.com>
2023-02-07 22:11:44 -0300

    tailwind + astro base project

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui"),
+		require('tailwind-scrollbar-hide')
+
+	],
+	daisyui: {
+		themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
+	  },
+	
+	
+}
