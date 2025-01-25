820fcf233654cac62c10771abf75c3fcfb8dd5dd
igoroliveira <igordeeoliveira@gmail.com>
2025-01-24 22:52:31 -0300

    new WIP site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://igoroliveira.dev',
-});
+export default defineConfig({});
